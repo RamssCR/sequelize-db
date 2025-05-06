650c6a6cdb1f5d967315eb7fc82d0979ca8bc25a
RamssCR <ramsesgpm@gmail.com>
2025-05-06 20:36:24 +0200

    fix: separated sequelize instance from the sync function

---
+++ src/configs/connect.js
@@ -0,0 +1,20 @@
+import '#models/author.model.js'
+import '#models/genre.model.js'
+import '#models/category.model.js'
+import '#models/user.model.js'
+import '#models/book.model.js'
+import { sequelize } from './database.config.js'
+
+/**
+ * Connect to the database and sync models
+ * @returns {Promise<void>} 
+ * @throws {Error} If unable to connect to the database
+ */
+export const connectDB = async () => {
+  try {
+    await sequelize.sync({ force: true })
+    console.log('Database connection has been established successfully.')
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
