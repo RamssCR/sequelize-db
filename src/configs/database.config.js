4cdba15c3e894178b3f9ec45779af18ecff71503
RamssCR <ramsesgpm@gmail.com>
2025-05-05 22:19:42 -0500

    style: prettified code

--- src/configs/database.config.js
+++ src/configs/database.config.js
@@ -1,23 +1,23 @@
 import { Sequelize } from 'sequelize'
 import {
-    DB_HOST,
-    DB_NAME,
-    DB_USER,
-    DB_PASSWORD,
-    DB_PORT
+  DB_HOST,
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  DB_PORT
 } from '#configs/env.config.js'
 
 export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    dialect: 'postgres',
-    host: DB_HOST,
-    port: DB_PORT,
+  dialect: 'postgres',
+  host: DB_HOST,
+  port: DB_PORT,
 })
 
 export const connectDB = async () => {
-    try {
-        await sequelize.sync()
-        console.log('Database connection has been established successfully.')
-    } catch (error) {
-        console.error('Unable to connect to the database:', error)
-    }
+  try {
+    await sequelize.sync()
+    console.log('Database connection has been established successfully.')
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
 }
