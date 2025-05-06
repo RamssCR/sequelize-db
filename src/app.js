624413dd9a8f40b6b6dac78c322517fb602f3a98
RamssCR <ramsesgpm@gmail.com>
2025-05-06 20:35:35 +0200

    feat: configured security measures to the API

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 import express, { json, urlencoded } from 'express'
-import { PORT } from '#configs/env.config.js'
+import { NODE_ENV, PORT } from '#configs/env.config.js'
+import { connectDB } from '#configs/connect.js'
 import { cors } from '#middlewares/cors.js'
-import parser from 'cookie-parser'
 import helmet from 'helmet'
+import parser from 'cookie-parser'
 import { router } from '#routes/index.js'
 
 const app = express()
 
 app.use(json())
-app.use(urlencoded({ extended: true }))
-app.use(cors())
+app.use(urlencoded({ extended: true, limit: NODE_ENV !== 'production' ? '50mb' : '100kb' }))
+app.use(cors)
 app.use(helmet())
 app.use(parser())
 app.use('/api/v1', router)
+
+connectDB()
 
 app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`))
--- src/configs/env.config.js
+++ src/configs/env.config.js
@@ -3,6 +3,7 @@ export const {
     JWT_SECRET,
     NODE_ENV,
     POSTGRES_URI,
+    ALLOWED_ORIGINS,
     DB_HOST,
     DB_NAME,
     DB_USER,
