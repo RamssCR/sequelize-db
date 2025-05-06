c71a54308eb955580c7e399da41f7755e37916b6
RamssCR <ramsesgpm@gmail.com>
2025-05-05 22:20:17 -0500

    feat: integrated cors, routes and cookie parser to the app builder

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express, { json, urlencoded } from 'express'
 import { PORT } from '#configs/env.config.js'
+import { cors } from '#middlewares/cors.js'
+import parser from 'cookie-parser'
 import helmet from 'helmet'
+import { router } from '#routes/index.js'
 
 const app = express()
 
 app.use(json())
 app.use(urlencoded({ extended: true }))
+app.use(cors())
 app.use(helmet())
+app.use(parser())
+app.use('/api/v1', router)
 
 app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`))
