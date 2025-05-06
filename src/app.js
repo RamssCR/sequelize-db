6d498436c466f0c512655ce8962f3a29d2a76fe4
RamssCR <ramsesgpm@gmail.com>
2025-05-06 23:57:27 +0200

    feat: added health check to the API

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express, { json, urlencoded } from 'express'
-import { NODE_ENV, PORT } from '#configs/env.config.js'
+import { PORT } from '#configs/env.config.js'
 import { connectDB } from '#configs/connect.js'
 import { cors } from '#middlewares/cors.js'
 import helmet from 'helmet'
@@ -9,12 +9,20 @@ import { router } from '#routes/index.js'
 const app = express()
 
 app.use(json())
-app.use(urlencoded({ extended: true, limit: NODE_ENV !== 'production' ? '50mb' : '100kb' }))
-app.use(cors)
+app.use(urlencoded({ extended: true }))
+app.use(cors())
 app.use(helmet())
 app.use(parser())
 app.use('/api/v1', router)
 
-connectDB()
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    message: 'Server is running smoothly',
+    uptime: process.uptime(),
+  })
+})
+
+await connectDB()
 
 app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`))
