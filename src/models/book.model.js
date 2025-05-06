7ff573cd14815dd6402b120d345552a2dac4fb7c
RamssCR <ramsesgpm@gmail.com>
2025-05-05 22:19:03 -0500

    feat: created Book model

---
+++ src/models/book.model.js
@@ -0,0 +1,77 @@
+import { DataTypes } from 'sequelize'
+import { Author } from '#models/author.model.js'
+import { Category } from '#models/category.model.js'
+import { Genre } from '#models/genre.model.js'
+import { User } from '#models/user.model.js'
+import { sequelize } from '#configs/database.config.js'
+
+export const Book = sequelize.define('Book', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  subtitle: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  synopsis: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  cover: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  pages: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  chapters: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  disabled: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  }
+}, {
+  timestamps: true,
+})
+
+// Associations
+Author.hasMany(Book)
+Book.belongsTo(Author, {
+  foreignKey: 'authorId',
+  as: 'author'
+})
+
+Category.hasMany(Book)
+Book.belongsTo(Category, {
+  foreignKey: 'categoryId',
+  as: 'category'
+})
+
+Genre.hasMany(Book)
+Book.belongsTo(Genre, {
+  foreignKey: 'genreId',
+  as: 'genre'
+})
+
+Book.belongsToMany(User, {
+  through: 'UserBook',
+  as: 'users'
+})
+
+User.belongsToMany(Book, {
+  through: 'UserBook',
+  as: 'books'
+})
