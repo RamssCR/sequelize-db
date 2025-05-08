0ac88c5a6da792860e823b920330e622dac5a69e
RamssCR <ramsesgpm@gmail.com>
2025-05-07 21:26:01 -0500

    feat: created login, profile and logout controllers

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,20 +1,24 @@
 import { User } from '#models/user.model.js'
 import { createToken } from '#libs/jwt.js'
-import { hash } from 'bcryptjs'
+import { hash, compare } from 'bcryptjs'
 
 /**
  * Register a new user when filling the required fields.
  * @param {import("express").Request} req - The request object.
  * @param {import("express").Response} res - The response object.
+ * @param {import("express").NextFunction} next - The next middleware function.
  * @returns {Promise<void>} A promise that resolves when the user is registered.
  * @throws {Error} If the user is already registered or if there is an error during registration.
  */
-export const register = async (req, res) => {
+export const register = async (req, res, next) => {
   const { username, email, password } = req.body
 
   try {
     const hashedPassword = await hash(password, 10)
-    const isExistingUser = await User.findOne({ where: { email } })
+    const isExistingUser = await User.findOne({ 
+      where: { email },
+      attributes: ['id', 'email', 'username']
+    })
 
     if (isExistingUser) return res.status(400).json({
       status: 'fail',
@@ -40,11 +44,7 @@ export const register = async (req, res) => {
       }
     })
   } catch (error) {
-    return res.status(500).json({
-      status: 'error',
-      message: 'internal server error',
-      error: error.message
-    })
+    next(error)
   }
 }
 
@@ -52,25 +52,97 @@ export const register = async (req, res) => {
  * Login a user with the provided credentials.
  * @param {import("express").Request} req - The request object.
  * @param {import("express").Response} res - The response object.
+ * @param {import("express").NextFunction} next - The next middleware function.
  * @returns {Promise<void>} A promise that resolves when the user is logged in.
  * @throws {Error} If the credentials are invalid or if there is an error during login.
  */
-export const login = async (req, res) => { }
+export const login = async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    const user = await User.findOne({ where: { email } })
+    if (!user) return res.status(400).json({
+      status: 'fail',
+      message: 'Incorrect email or password'
+    })
+
+    const isPasswordValid = await compare(password, user.dataValues.password)
+    if (!isPasswordValid) return res.status(400).json({
+      status: 'fail',
+      message: 'Incorrect email or password'
+    })
+
+    const token = await createToken({ id: user.dataValues.id })
+    return res.status(200).cookie('token', token).json({
+      status: 'success',
+      message: 'user logged in successfully',
+      data: {
+        user: {
+          id: user.dataValues.id,
+          username: user.dataValues.username,
+          email: user.dataValues.email
+        }
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
+}
 
 /**
  * Get the profile of the logged-in user.
  * @param {import("express").Request} req - The request object.
  * @param {import("express").Response} res - The response object.
+ * @param {import("express").NextFunction} next - The next middleware function.
  * @returns {Promise<void>} A promise that resolves when the profile is retrieved.
  * @throws {Error} If there is an error during profile retrieval.
  */
-export const profile = async (req, res) => { }
+export const profile = async (req, res, next) => {
+  const { id } = req.user
+
+  try {
+    const user = await User.findOne({
+      where: { id },
+      attributes: ['id', 'username', 'email']
+    })
+
+    if (!user) return res
+      .cookie('token', '', { expires: new Date(0) })
+      .status(401).json({
+        status: 'fail',
+        message: 'Invalid session'
+      })
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'user profile retrieved successfully',
+      data: {
+        user: {
+          id: user.dataValues.id,
+          username: user.dataValues.username,
+          email: user.dataValues.email
+        }
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
+}
 
 /**
  * Logout the user by clearing the session or token.
  * @param {import("express").Request} req - The request object.
  * @param {import("express").Response} res - The response object.
+ * @param {import("express").NextFunction} next - The next middleware function.
  * @returns {Promise<void>} A promise that resolves when the user is logged out.
  * @throws {Error} If there is an error during logout.
  */
-export const logout = async (req, res) => { }
+export const logout = async (req, res, next) => {
+  try {
+    return res
+      .cookie('token', '', { expires: new Date(0) })
+      .sendStatus(204)
+  } catch (error) {
+    next(error)
+  }
+}
