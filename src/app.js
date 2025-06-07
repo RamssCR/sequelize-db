e1a2bb72c52dd4736f9f34308dc3c05ce4644bc0
RamssCR <ramsesgpm@gmail.com>
2025-06-07 15:33:46 -0500

    fix: enabled content sharing protocol

--- src/app.js
+++ src/app.js
@@ -12,7 +12,9 @@ const app = express()
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(cors())
-app.use(helmet())
+app.use(helmet({
+  contentSecurityPolicy: false
+}))
 app.use(parser())
 app.use('/api/v1', router)
 app.use(errorHandler)
