7f8be49cac6e75036d336a518333872f2d41f09e
RamssCR <ramsesgpm@gmail.com>
2025-06-17 19:44:03 -0500

    feat: added new shelf route

--- src/routes/shelves/shelf.routes.js
+++ src/routes/shelves/shelf.routes.js
@@ -3,6 +3,7 @@ import { validateToken } from '#middlewares/validateToken.js'
 import {
   addBookToShelf,
   getAllShelfBooks,
+  getShelfBooksIds,
   getShelfBookBySlug,
   removeBookFromShelf
 } from '#controllers/shelf.controller.js'
@@ -11,6 +12,9 @@ export const shelfRouter = Router()
 
 // Get all books from the user's shelf
 shelfRouter.get('/', validateToken, getAllShelfBooks)
+
+// Get all book IDs from the user's shelf
+shelfRouter.get('/ids', validateToken, getShelfBooksIds)
 
 // Get a book by slug from the user's shelf
 shelfRouter.get('/:slug', validateToken, getShelfBookBySlug)
