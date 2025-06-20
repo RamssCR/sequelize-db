61d0e0e6bfcc7eb9502c1da77a07c8f3a3bd3784
RamssCR <ramsesgpm@gmail.com>
2025-06-19 21:20:55 -0500

    feat: integrated params validation as a middleware

--- src/routes/books/chapter.routes.js
+++ src/routes/books/chapter.routes.js
@@ -3,9 +3,11 @@ import {
   getBookChaptersBySlug,
   getBookChapterByNumber
 } from '#controllers/chapter.controller.js'
+import { chapterQuerySchema } from '#schemas/chapterQuerySchema.js'
+import { validateSchema } from '#middlewares/validateSchema.js'
 import { validateToken } from '#middlewares/validateToken.js'
 
 export const chapterRouter = Router()
 
 chapterRouter.get('/:slug', validateToken, getBookChaptersBySlug)
-chapterRouter.get('/:slug/:number', validateToken, getBookChapterByNumber)
+chapterRouter.get('/:slug/:number', validateToken, validateSchema(chapterQuerySchema, "params"), getBookChapterByNumber)
