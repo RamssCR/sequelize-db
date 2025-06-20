1ee83148448e25540a93b8a273d8c96060e49025
RamssCR <ramsesgpm@gmail.com>
2025-06-19 20:41:43 -0500

    feat: addded chapter to the seeder script

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,9 +1,11 @@
 import { Genre } from '#models/genre.model.js'
 import { Author } from '#models/author.model.js'
 import { Book } from '#models/book.model.js'
+import { Chapter } from '#models/chapter.model.js';
 import genres from '#seeders/genres.json' with { type: 'json' }
 import authors from '#seeders/authors.json' with { type: 'json' }
 import books from '#seeders/books.json' with { type: 'json' }
+import chapters from '#seeders/chapters.json' with { type: 'json' }
 
 await Genre.bulkCreate(genres)
 await Author.bulkCreate(authors)
@@ -41,4 +43,26 @@ const transformedBooks = books.map(book => {
 }).filter(book => book !== null)
 
 await Book.bulkCreate(transformedBooks, { ignoreDuplicates: true })
+
+// Chapter seeding
+const booksFromDB = await Book.findAll()
+const bookMap = new Map(booksFromDB.map((b) => [b.dataValues.slug, b.dataValues.id]))
+const transformedChapters = chapters.map(chapter => {
+  const bookId = bookMap.get(chapter.bookSlug)
+
+  if (!bookId) {
+    console.error(`Error: Missing mapping for chapter: ${chapter.title}`)
+    return null
+  }
+
+  return {
+    title: chapter.title,
+    chapterNumber: chapter.chapterNumber,
+    content: chapter.content,
+    slug: chapter.bookSlug,
+    BookId: bookId,
+  }
+}).filter(chapter => chapter !== null)
+
+await Chapter.bulkCreate(transformedChapters, { ignoreDuplicates: true })
 console.log('Database seeded successfully')
